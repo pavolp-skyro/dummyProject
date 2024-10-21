ac5fa2de865c0074ca2de3c75f4f98aa071c61f3
Rokatoni <tobias.kurz12345@gmail.com>
2024-10-21 14:40:01 +0200

    Hra

--- Assets/Tobi/gta5.cs
+++ Assets/Tobi/gta5.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class gta5 : MonoBehaviour
 {
+
+    public Rigidbody2D Player;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class gta5 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if(Input.GetKeyDown(KeyCode.W)) {
+        Player.velocity = new Vector3(0f, 100f, 0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Player.velocity = new Vector3(100f, 0f, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+            {
+                Player.velocity = new Vector3(-100f, 0f, 0f);
+            }
+        }
 }
